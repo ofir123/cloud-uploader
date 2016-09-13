00294d6a0329c6fb55a7864cd3dacdf575ef4cd7
ofir123 <ofirbrukner@gmail.com>
2016-09-14 01:49:11 +0300

    Fixed cloud path generation.

--- amazonuploader/uploader.py
+++ amazonuploader/uploader.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import sys
+import posixpath
 
 import logbook
 from guessit import guessit
@@ -53,7 +54,8 @@ def upload_file(file_path):
             if language_extension in LANGUAGE_EXTENSIONS:
                 fixed_file_path = fixed_file_name + DEFAULT_VIDEO_EXTENSION
     # Create cloud path based on guessit results.
-    cloud_path = None
+    cloud_dir = None
+    cloud_file = None
     guess_results = guessit(os.path.basename(fixed_file_path))
     video_type = guess_results.get('type')
     title = guess_results.get('title')
@@ -62,19 +64,24 @@ def upload_file(file_path):
         if season:
             episode = guess_results.get('episode')
             if episode:
-                cloud_path = '{}/{}/Season {:02d}/{} - S{:02d}E{:02d}'.format(
-                    config.TV_PATH, title, season, title, season, episode)
+                cloud_dir = '{}/{}/Season {:02d}'.format(config.TV_PATH, title, season)
+                cloud_file = '{} - S{:02d}E{:02d}'.format(title, season, episode)
     elif video_type == 'movie' and title:
         year = guess_results.get('year')
         if year:
-            cloud_path = '{}/{} ({})/{} ({})'.format(config.MOVIE_PATH, title, year, title, year)
-    if cloud_path:
+            cloud_dir = '{}/{} ({})'.format(config.MOVIE_PATH, title, year)
+            cloud_file = '{} ({})'.format(title, year)
+    if cloud_dir and cloud_file:
         if language_extension:
-            cloud_path += language_extension
-        cloud_path += file_extension
-        logger.info('Cloud path: {}'.format(cloud_path))
+            cloud_file += language_extension
+        cloud_file += file_extension
+        logger.info('Cloud path: {}'.format(posixpath.join(cloud_dir, cloud_file)))
+        # Rename local file before upload.
+        base_dir = os.path.dirname(file_path)
+        new_path = os.path.join(base_dir, cloud_file)
+        os.rename(file_path, new_path)
         # Upload!
-        process = subprocess.run([config.ACD_CLI_PATH, 'upload', file_path, cloud_path], shell=True,
+        process = subprocess.run([config.ACD_CLI_PATH, 'upload', new_path, cloud_dir], shell=True,
                                  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         # Check results.
         if process.returncode != 0:
