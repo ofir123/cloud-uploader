d41b5e270ea14ab9b0c4aa5d84e7f733cc3b457f
ofir123 <ofirbrukner@gmail.com>
2017-06-26 23:08:27 +0300

    Added Sonarr faker.

---
+++ scripts/sonarr_faker.py
@@ -0,0 +1,56 @@
+#!/usr/local/bin/python3
+import os
+import sys
+from pathlib import Path
+
+import logbook
+
+# Directories settings.
+TV_ROOT_PATH = '/mnt/vdb/plexdrive/gdrive_decrypted/TV'
+FAKE_ROOT_PATH = '/mnt/vdb/fake/TV'
+LOG_FILE_PATH = '/var/log/sonarr_faker.log'
+
+logger = logbook.Logger(__name__)
+
+
+def _get_log_handlers():
+    """
+    Initializes all relevant log handlers.
+
+    :return: A list of log handlers.
+    """
+    return [
+        logbook.NullHandler(),
+        logbook.StreamHandler(sys.stdout, level=logbook.INFO, bubble=True),
+        logbook.RotatingFileHandler(LOG_FILE_PATH, level=logbook.DEBUG, max_size=5 * 1024 * 1024, backup_count=1,
+                                    bubble=True)
+    ]
+
+
+def main():
+    """
+    Go over the entire media collection, and check for weird files.
+    """
+    with logbook.NestedSetup(_get_log_handlers()).applicationbound():
+        logger.info('Sonarr faker started!')
+        # Verify root path.
+        if not os.path.isdir(TV_ROOT_PATH):
+            raise FileNotFoundError('Couldn\'t find TV root directory! Stopping...')
+        # Create destination path.
+        if not os.path.exists(FAKE_ROOT_PATH):
+            os.makedirs(FAKE_ROOT_PATH)
+        # Start working!
+        for root, dirs, files in os.walk(TV_ROOT_PATH):
+            logger.info('Handling dir: {}'.format(root))
+            # Create a fake directory.
+            fake_root = root.replace(TV_ROOT_PATH, FAKE_ROOT_PATH)
+            os.makedirs(fake_root)
+            for f in files:
+                # Create a fake file.
+                fake_path = os.path.join(fake_root, f)
+                Path(fake_path).touch()
+        logger.info('All done!')
+
+
+if __name__ == '__main__':
+    main()
